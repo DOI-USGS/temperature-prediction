e346f598f127a8930defe84d8d55eee2f49c3f81
ellenbechtel <ellen.bechtel@gmail.com>
2020-10-27 17:03:28 -0400

    inserted IntroMap svg to Chapter 1 Importance

--- src/views/1-Importance/Importance.vue
+++ src/views/1-Importance/Importance.vue
@@ -3,10 +3,18 @@
       <h2>Importance</h2>
       <p>We'll place all the content for Chapter 1 - Importance - in here!</p>
       <p>Try clicking around on the nav bar up top to go to other parts of the viz.</p>
+    <IntroMap />
   </div>
 </template>
 
 <script>
+
+  export default {
+    name: "Importance",
+    components: {
+      IntroMap: () => import( /* webpackPrefetch: true */ /*webpackChunkName: "intromap"*/ "./../../components/IntroMap"),
+    }
+  }
 </script>
 
 <style scoped lang="scss">
