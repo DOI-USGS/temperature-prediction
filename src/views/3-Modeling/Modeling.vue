fa4fc445c8bf541d8041309c0fc91122df8fe192
coll nell <collnellphd@gmail.com>
2020-10-27 11:57:03 -0400

    basic script setup

--- src/views/3-Modeling/Modeling.vue
+++ src/views/3-Modeling/Modeling.vue
@@ -10,11 +10,46 @@
 </template>
 
 <script>
+    import * as d3Base from "d3";
+    // polyfill for IntersectionObserver
+    import 'intersection-observer';
+    import scrollama from 'scrollama';
 
   export default {
     name: 'Modeling',
     components: {
-    }
+    },
+    data() {
+          return {
+            publicPath: process.env.BASE_URL, // this is need for the data files in the public folder, this allows the application to find the files when on different deployment roots
+            d3: null, // this is used so that we can assign d3 plugins to the d3 instance
+            // global variables instantiated in next section
+          }
+        },
+        mounted() {
+          this.d3 = Object.assign(d3Base, { geoScaleBar, geoScaleBottom, geoScaleTop, geoScaleKilometers, geoScaleMiles }); // this loads d3 plugins with webpack
+
+          this.setPanels(); //begin script when window loads
+          this.setScrolls();
+        },
+        //methods are executed once, not cached as computed properties, rerun everytime deal with new step
+        methods: {
+
+          setPanels() {
+            const self = this;
+            //code to run on load
+             
+          },
+          callback(data) {
+          // call functions
+            this.setScrolls();
+
+          },
+           
+          setScrolls() {
+            
+          }
+        }
   }
 </script>
 
