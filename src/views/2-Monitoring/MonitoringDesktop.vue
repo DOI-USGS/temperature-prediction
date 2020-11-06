1418a829fb09f19b5e46db8b2ec11ad8b5fd3e44
ellenbechtel <ellen.bechtel@gmail.com>
2020-11-06 11:54:57 -0500

    load mobile-detection

--- src/views/2-Monitoring/MonitoringDesktop.vue
+++ src/views/2-Monitoring/MonitoringDesktop.vue
@@ -2,6 +2,7 @@
   <div id="monitoring-desktop">
     <div class="text-content">
       <h2>Monitoring!</h2>
+      <div class="hello">{{ msg }}</div>
       <p>Aaaaaand here's where we'll put all the code for ✨Chapter 2.✨  See the components loaded below here?</p>
     </div>  
     <Set2 />
@@ -10,13 +11,21 @@
 
 <script>
   import Set2 from "./../set2/Set2";
-
+  import { isMobile } from 'mobile-device-detect';
+  
   export default {
     name: 'MonitoringDesktop',
     components: {
       Set2
+    },
+    data() {
+        return {
+            msg: isMobile ? "I'm on mobile!" : "I'm on desktop!",
+        }
     }
   }
+
+          
 </script>
 
 <style scoped lang="scss">
