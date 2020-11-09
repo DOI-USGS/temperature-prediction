c135a97e30575fa6b2cf19eb7c64331b6dcbad44
ellenbechtel <ellen.bechtel@gmail.com>
2020-11-09 15:18:35 -0500

    input all dynamic text into mobile layout

--- src/assets/text/monitoringText.js
+++ src/assets/text/monitoringText.js
@@ -2,6 +2,18 @@ export default {
    textContents: {
         title1: "What we know, and what we don't",
         paragraph1: "On the surface, it appears that the USGS has a lot of information about stream temperature in the Basin.",
-        paragraph2: "This map shows all of the sites where temperature has been measured at any point in the last 40 years."
+        paragraph2: "This map shows all of the sites where temperature has been measured at any point in the last 40 years.",
+        paragraph3: "The USGS has been monitoring temperature in the Delaware River Basin since YYYY, and has amased over ### site-days worth of temperature measurements.",
+        paragraph4: "The recent uptick in temperature monitoring is due to the USGS's Next Generation Water Observing Systems (NGWOS).  NGWOSis a targeted effort in the Basin to develop and apply new monitoring techniques for a wide range of water characteristics in the U.S.  Temperature can now be monitored by boat, by camera, by drone, by cable, by satellite, by sensor, or by the tried and true methods of a field technician sticking a thermometer into the water.",
+        title2: "But don't let the sheer amount of data fool you.  Most temperature records are concentrated in a few sites across the Basin.",
+        paragraph5: "Most data in the basin comes from a small number of sites.  The top 1% of most-measured river reaches - only about 5 sites - make up nearly 20% of the data.  And there are whole chunks of time and space where we don't know anything about the river, where we're literally 'in the dark.'",
+        paragraph6: "Watch the animation here lights up the reaches where data has shed light on stream temperature in the Delaware River in each of the last forty years.",
+        paragraph7: "The amount of data in each reach per year varies widely, with some reaches measured once a year, and some reaches measured every day.",
+        paragraph8: "We can represent the availability of data as a matrix with two axes.",
+        paragraph9: "This allows us to see the chunks of time and space where we are in the dark about stream temperature.",
+        paragraph10: "Our understanding of temperature dyanmics in the Basin is even more complicated than this - streams don't all behave the same way.  Features of the landscape can have large effects on water temperature that create differences in temperature dynamics.  Groundwater inputs, reservoirs, and urban areas all have distinct temperature signatures.",
+        paragraph11: "// Say some thing explanatory about this line chart //",
+        paragraph12: "Explore a year of actual temperature data across all monitored sites in the Delaware River Basin.",
+        paragraph13: "Data sparsity and variability in stream temperature across the network limits our availability to answer questions like: How far downstream will cold water measured here affec temperature?  How has fish habitat changed through time?  Will we have enough cold reservoir in the reservoir in 50 years?"
     }
 }
--- src/views/2-Monitoring/MonitoringMobile.vue
+++ src/views/2-Monitoring/MonitoringMobile.vue
@@ -7,21 +7,55 @@
       <p v-html="text.paragraph1" />
       <p v-html="text.paragraph2" />
     </div>   
-    <Set1 />
     <MonitoringMap1 />
+    <div class="text-content">
+      <p v-html="text.paragraph3" />
+      <p>BAR CHART HERE</p>
+      <!-- <MonitoringBarChart /> -->
+      <p v-html="text.paragraph4" />
+      <p>ICONS HERE</p>
+      <!-- <Chapter2Icons1 /> -->
+      <p v-html="text.title2" />
+      <p v-html="text.paragraph5" />
+      <p v-html="text.paragraph6" />
+    </div>
+    <h3>ANIMATED MAP HERE</h3>
+    <!-- <MonitoringMap2 /> -->
+    <div class="text-content">
+      <p v-html="text.paragraph7" />
+      <p v-html="text.paragraph8" />
+    </div>
+    <!-- <MatrixExplainerSpace /> -->
+    <!-- <MatrixExplainerTime /> -->
+    <!-- <MatrixExplainerCells /> -->
+    <div class="text-content">
+      <p v-html="text.paragraph9" />
+    </div>
+    <!-- <MatrixAvailability /> -->
+    <div class="text-content">
+      <p v-html="text.paragraph10" />
+      <!-- <Chapter2Icons2 /> -->
+    </div>
+    <!-- <MonitoringLineChart /> -->
+    <div class="text-content">
+      <p v-html="text.paragraph11" />
+      <p v-html="text.paragraph12" />
+    </div>
+    <!-- <MonitoringMatrixTemperature /> -->
+    <div class="text-content">
+      <p v-html="text.paragraph13" />
+    </div>
   </div>
 </template>
 
 <script>
   import monitoringText from "./../../assets/text/monitoringText";
-  import Set1 from "./../set1/Set1";
   import MonitoringMap1 from "./../../components/2-Monitoring-Mobile/MonitoringMap1";
   import { isMobile } from 'mobile-device-detect';
   
   export default {
     name: 'MonitoringMobile',
     components: {
-      Set1,
       MonitoringMap1
     },
     data() {
