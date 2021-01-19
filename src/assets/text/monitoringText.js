02bcf8e775cc298b237770d9d9df5328698f149c
Samantha Oliver <soliver@usgs.gov>
2021-01-19 13:30:19 -0600

    suggested text changes in "monitoring"

--- src/assets/text/monitoringText.js
+++ src/assets/text/monitoringText.js
@@ -1,26 +1,23 @@
 export default {
    textContents: {
         title1: "What we know, and what we don't",
-        paragraph1: "At first glance, it appears that the USGS has a lot of information about stream temperature in the Basin.",
-        paragraph2: "This map shows medium to large streams within the basin, along with all of the sites on those streams where temperature has been measured at any point in the last 118 years.",
+        paragraph1: "At first glance, it appears that the USGS has a lot of information about stream temperature in the Basin. The map below shows medium to large streams and all places where temperature has ever been measured measured (white dots).",
         paragraph3: "The USGS has been monitoring temperature in the Delaware River Basin since 1901, and has amassed over 450,000 site-days worth of temperature measurements. Temperature monitoring really picked up in the mid-1960s, and has grown considerably in recent decades.",
-        paragraph4: "Since 2017, an uptick in temperature monitoring can be attributed to the USGS's Next Generation Water Observing Systems (NGWOS).",
-        paragraph5: "NGWOS is a targeted effort in the basin to develop and apply new monitoring techniques for a wide range of water characteristics. Temperature is one of the most widely measured water characteristics in the U.S. – temperature can now be monitored by boat, by camera, by drone, by cable, by satellite, by sensor, or by the tried and true method of a field technician sticking a thermometer into the water.",
+        paragraph4: "Since 2017, an uptick in temperature monitoring can be attributed to the USGS's Next Generation Water Observing Systems (NGWOS). NGWOS is a targeted effort in the basin to develop and apply new monitoring techniques for a wide range of water characteristics.",
+        paragraph5: "Temperature is one of the most widely measured water characteristics in the U.S. – temperature can now be monitored by boat, by camera, by drone, by cable, by satellite, by sensor, or by the tried and true method of a field technician sticking a thermometer into the water.",
         title2: "Data Availability",
         subtitle2: "But don't let the sheer amount of data fool you.  Most temperature records are concentrated at a few sites across the basin.",
-        paragraph6: "The top 1% of measured river reaches - only about 5 sites - makes up nearly 20% of all data.  And there are whole chunks of space and time where we don't know anything about the river, where we're 'in the dark.'",
+        paragraph6: "The top 1% of measured river reaches - only about 5 sites - makes up nearly 20% of all data. There are whole chunks of space and time where we don't know anything about the river, where we're 'in the dark.'",
         tiptextMobile: "Watch as the animation lights up the reaches for which there is stream temperature data in each of the last forty years.",
         tiptextDesktop: "Explore the availability of temperature observations across the Delaware River Basin by selecting a river reach (on the left) or a year (on the right) to explore where and when we have the most information.",
-        paragraph8: "The amount of data per reach per year varies widely, with some reaches measured once a year, and some reaches measured every day.",
-        paragraph9: "We can represent the availability of data as a matrix with two axes and one color scale.",
-        title3: "Real Temperature Dynamics",
+        paragraph8: "In the figure below, we've simplified the variability in the amount of data in time and space into a matrix.",
+        title3: "Water Temperature Dynamics",
         paragraph10: "Our understanding of temperature dynamics in the basin gets even more complicated by the fact than not all streams behave the same way. Features of the landscape can have large effects on water temperature that create differences in temperature dynamics. Groundwater inputs, reservoirs, and urban areas all have distinct temperature signatures.",
         paragraph11: "If we look at a single year of data, we can see that stream temperatures are not uniform throughout the basin.",
         paragraph12: "In 2019, an urban reach in Philadelphia was the warmest stream reach in the basin, while a reach below the Beltzville Resevoir was the coolest. These reaches aren't that far from eachother as the crow flies, but they experience very different temperature regimes.",
         paragraph13: "Explore a year of actual temperature data across all monitored sites on medium to large streams in the Delaware River Basin.",
         title4: "Implications for Temperature Modeling",
         paragraph14: "Data sparsity and variability in stream temperature across the network limit our ability to answer questions like: How far downstream will a cold water release affect temperature?  How has fish habitat changed through time?  Will we have enough cold water in the reservoir in 50 years?",
-        paragraph15: "These challenges are not unique to the Delaware River Basin. In fact, relative to the rest of the United States, the DRB is one of the most highly monitored bains in the country. How can we create models that are robust to a diversity of temperature dynamics, in places and times where we don't have an abundance of information?",
-        paragraph16: "These challenges -- leverging existing data, capturing diverse cause-and-effect relationships, prediction in unmonitored systems and at broad scales -- require innovation in modeling. The USGS, along with our academic computer science partners, have been developing new modeling techniques called \"process-guided deep learning\"."
+        paragraph15: "These challenges are not unique to the Delaware River Basin. In fact, relative to the rest of the United States, the DRB is one of the most highly monitored bains in the country. How can we create models that are robust to a diversity of temperature dynamics, in places and times where we don't have an abundance of information? Visit the Modeling section to find out more about innovative modeling techniques to predict water temperature.",
     }
 }
