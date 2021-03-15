08383342613447f67cd104f30df3668d2a809a5c
Corson-Dosch <hcorson-dosch@usgs.gov>
2021-03-15 10:41:54 -0500

    fix styling of bottom chapter links

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,33 +1,15 @@
 <template>
   <nav id="nav">
     <div>
-      <!-- p class="chapter">
-        Go to Chapter
-      </p -->
-      <!-- router-link
-        to="/"
-        @click.native="$scrollToTop"
-      >
-        1. Importance
-      </router-link -->
       <p>
         <router-link
           to="/monitoring"
           @click.native="scrollToTop"
-        >
-          Monitor
-        </router-link> <router-link
+        >Monitor</router-link>   <router-link
           to="/modeling"
           @click.native="scrollToTop"
-        >
-          Model
-        </router-link>
+        >Model</router-link>
       </p>
-    </div>
-    <div>
-      <!-- <p class="nav-title">
-        Choose a chapter
-      </p> -->
     </div>
   </nav>
 </template>
@@ -39,24 +21,6 @@
           return {
             currentLinks: null,
           }
-        },
-        mounted() {
-            // this.setPanels();
-        },
-        methods: {
-          // setPanels() {
-          //   this.currentLinks = document.querySelectorAll('a[href="#/'+document.URL.split('/').pop()+'"]')
-          //   console.log("current links")
-          //   console.log(this.currentLinks)
-          //   this.currentLinks.forEach(function(link) {
-          //       console.log('classList')
-          //       console.log(link.classList)
-          //       // link.className += ' current-link'
-          //       link.classList.add('current-link')
-          //       console.log('classList2')
-          //       console.log(link.classList)
-          //   });
-          // }
         }
     }
 </script>
@@ -87,51 +51,33 @@ $monotoneBlue5: #10313e;
 $monotoneBlueTransparent: rgba(76,101,110, .2);
 
 
-  // .current-link {
-  //   color: $plasmaYellow;
-  // }
   #nav a.router-link-active {
     color: $plasmaYellow;
     box-shadow: 0 2px 0 $plasmaYellow;
+    padding: 0px 5px;
   }
 
   #nav {
     text-align: center;
-    padding: 40px 0 40px 0;
-  }
-  @media (max-width: 600px) {
-    #nav a {
-      font-size: .7em;
-    }
-  }
-
-
-  .nav-title {
-    color: $offWhite;
-    font-family: 'Georgia', Helvetica, Arial, sans-serif;
-    line-height: 1.4;
-    // text-align: center;
-    // font-size: .8em;
-    // font-style: italic;
-    // margin-top: 10px;
+    padding: 20px 0 40px 0;
   }
 
   #nav a {
+    font-family: 'Georgia', Helvetica, Arial, sans-serif;
     font-weight: 100;
     text-align: center;
     box-shadow: 0 0.5px 0 #9c9c9c;
     color: #9c9c9c;
     position: relative;
-    padding: 0px 3px;
+    padding: 0px 5px;
     margin: 0px 7px;
     text-decoration: none;
-    // text-transform: uppercase;
+    font-size: 20pt;
+    @media (max-width: 600px) {
+      font-size: 16pt;
+    }
   }
 
-  // #nav a:visited {
-  //   font-weight: 400;
-  //   color: $plasmaYellow;
-  // }
   #nav a:focus {
     color: $plasmaYellow;
     outline: none;
@@ -143,15 +89,7 @@ $monotoneBlueTransparent: rgba(76,101,110, .2);
     background-color: $plasmaYellow;
     border-radius: 5px;
     padding: 5px;
-    // transition: background-color .2s ease;
     cursor: pointer;
   }
-
-  #nav a:active {
-    box-shadow: none;
-    top: 5px;
-  }
-
- 
 
 </style>
