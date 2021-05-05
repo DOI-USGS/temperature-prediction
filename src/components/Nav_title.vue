e7faed8353cb1ba998fc492776f1b6855d9cdf61
Corson-Dosch <hcorson-dosch@usgs.gov>
2021-05-05 18:08:59 -0500

    fix nav styling after running lint

--- src/components/Nav_title.vue
+++ src/components/Nav_title.vue
@@ -7,14 +7,10 @@
             How we<router-link
               to="/monitoring"
               @click.native="scrollToTop"
-            >
-              Monitor
-            </router-link><span class="dim_text">|</span><router-link
+            >Monitor</router-link><span class="dim_text">|</span><router-link
               to="/modeling"
               @click.native="scrollToTop"
-            >
-              Model
-            </router-link><br>Stream Temperature in the<br>
+            >Model</router-link><br>Stream Temperature in the<br>
             Delaware River Basin
           </h1>
           <br>
