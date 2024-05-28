178670b309050195e0ecd71aa8987745a4bd4a14
Corson-Dosch <hcorson-dosch@usgs.gov>
2024-05-28 16:07:32 -0500

    fix i declarations throughout

--- src/views/1-Monitoring/MonitoringDesktop.vue
+++ src/views/1-Monitoring/MonitoringDesktop.vue
@@ -470,7 +470,7 @@
         // Function to process data for Ch 1 part 1 bar chart
         type(d, i, columns) {
           let t = 0;
-          for (i = 1, t; i < columns.length; ++i)
+          for (let i = 1, t; i < columns.length; ++i)
             // for each row, which is d, cycle through the columns
             t += d[columns[i]] = +d[columns[i]];
           // create a new column in the data titled "total"
@@ -914,7 +914,7 @@
           // build array of all values of observation counts
           let domainArrayTemporalCounts = [];
           let i;
-          for (i=0; i<csv_matrix_annual.length; i++){
+          for (let i = 0; i<csv_matrix_annual.length; i++){
             let val = parseFloat(csv_matrix_annual[i]['obs_count']);
             domainArrayTemporalCounts.push(val);
           };
@@ -929,7 +929,7 @@
 
           // build list of posible counts (0 to 366)
           let count_list = [];
-          for (i = 1; i <= self.temporalCountMax_c1p2; i++) {
+          for (let i = 1; i <= self.temporalCountMax_c1p2; i++) {
             count_list.push(i);
           } 
 
@@ -1231,7 +1231,7 @@
           
           // build array of all values of observed temperature
           let arrayObsTemps = [];
-          for (i=0; i<csv_matrix_daily_2019.length; i++){
+          for (let i = 0; i<csv_matrix_daily_2019.length; i++){
             let val = parseFloat(csv_matrix_daily_2019[i]['mean_temp_c']);
             if (val){
               arrayObsTemps.push(val);
@@ -1253,7 +1253,7 @@
 
           // build list of posible temperatures
           let temp_list = [];
-          for (i = obsTempMin; i <= obsTempMax; i++) {
+          for (let i = obsTempMin; i <= obsTempMax; i++) {
             temp_list.push(i);
           } 
 
